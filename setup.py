2f2105685e3ceb2dc900bf177d820dae6b4a2064
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 16:54:45 +0100

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.rst") as f:
     readme = f.read()
 
 setup(
     name="oommfodt",
-    version="0.1",
+    version="0.5.1",
     description="A Python package for reading and analysing OOMMF odt files",
     long_description=readme,
     author="Computational Modelling Group",
     author_email="fangohr@soton.ac.uk",
     url="https://github.com/joommf/oommfodt",
-    download_url="https://github.com/joommf/oommfodt/tarball/0.1",
+    download_url="https://github.com/joommf/oommfodt/tarball/0.5.1",
     packages=["oommfodt",
               "oommfodt.tests"],
     install_requires=["pandas",
                       "openpyxl",
                       "xlrd",
                       "xlwt"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-    ]
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 3"]
 )
