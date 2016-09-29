6eb362d7b41ef47e0f26d32d1e24796ec1f8d171
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 13:14:30 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from distutils.core import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 setup(
-    name='oommfodt',
-    version='0.1',
-    description='A Python package for reading and analysing OOMMF odt files',
+    name="oommfodt",
+    version="0.1",
+    description="A Python package for reading and analysing OOMMF odt files",
     long_description=readme,
-    author='Computational Modelling Group',
-    author_email='fangohr@soton.ac.uk',
-    url = 'https://github.com/joommf/oommfodt',
-    download_url = 'https://github.com/joommf/oommfodt/tarball/0.1',
-    packages=['oommfodt', 'oommfodt.tests'],
+    author="Computational Modelling Group",
+    author_email="fangohr@soton.ac.uk",
+    url="https://github.com/joommf/oommfodt",
+    download_url="https://github.com/joommf/oommfodt/tarball/0.1",
+    packages=["oommfodt",
+              "oommfodt.tests"],
+    install_requires=["pandas"],
     classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ]
 )
