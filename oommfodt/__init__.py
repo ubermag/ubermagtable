39f4568f844a915acfdcea8d01df2f5aa03c6533
mb1a15 <mb4e10@soton.ac.uk>
2016-10-13 17:05:25 +0100

    Fix.

--- oommfodt/__init__.py
+++ oommfodt/__init__.py
@@ -3,4 +3,4 @@ from .oommfodt import OOMMFodt
 
 def test():
     import pytest  # pragma: no cover
-    pytest.main()  # pragma: no cover
+    pytest.main(["--pyargs", "oommfodt"])  # pragma: no cover
