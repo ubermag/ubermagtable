8e973228c5913b28e786564b0c9e198d314e1a60
mb1a15 <mb4e10@soton.ac.uk>
2016-10-14 16:07:20 +0100

    Make tests more informative.

--- oommfodt/__init__.py
+++ oommfodt/__init__.py
@@ -3,4 +3,4 @@ from .oommfodt import OOMMFodt
 
 def test():
     import pytest  # pragma: no cover
-    pytest.main(["--pyargs", "oommfodt"])  # pragma: no cover
+    pytest.main(["-v", "--pyargs", "oommfodt"])  # pragma: no cover
