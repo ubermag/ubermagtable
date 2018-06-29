0c8a3bc868200cef7ce5e5a3ec21f40dfb6c7ce9
Marijan Beg <m.beg@soton.ac.uk>
2018-06-29 22:06:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.rst") as f:
+with open("README.md") as f:
     readme = f.read()
 
 setuptools.setup(
@@ -9,7 +9,7 @@ setuptools.setup(
     description="A Python package for reading and analysing OOMMF .odt files",
     long_description=readme,
     url='https://joommf.github.io',
-    author='Marijan Beg, Ryan A. Pepper, and Hans Fangohr',
+    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=["pandas",
