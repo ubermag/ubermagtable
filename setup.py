6d109e585b44dbb2280820bacaee3a42e834c646
Marijan Beg <m.beg@soton.ac.uk>
2018-06-30 19:25:19 +0200

    Update setup.py for Markdown README file.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import setuptools
 
-with open("README.md", 'r+', encoding="utf-8") as f:
-    readme = f.read()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="oommfodt",
     version="0.8",
     description="Python package for reading and analysis of OOMMF .odt files.",
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
@@ -19,7 +20,11 @@ setuptools.setup(
     package_data={'oommfodt.tests': ["test_odt_files/*.odt"]},
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
                  'Topic :: Scientific/Engineering :: Physics',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3 :: Only']
+                 'Natural Language :: English']
 )
