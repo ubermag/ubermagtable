555b23ca0c484f29bea78a3c6866f67b9ccfd8f1
Marijan Beg <marijan.beg@xfel.eu>
2018-07-01 19:43:40 +0200

    Expose pytest.

--- oommfodt/__init__.py
+++ oommfodt/__init__.py
@@ -1,9 +1,9 @@
+import pytest
 import pkg_resources
 from .oommfodt import read
 
 
 def test():
-    import pytest  # pragma: no cover
     return pytest.main(["-v", "--pyargs", "oommfodt"])  # pragma: no cover
 
 __version__ = pkg_resources.get_distribution(__name__).version
