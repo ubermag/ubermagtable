041f5ea28c178987d93ed741940ccc736a9b8252
Marijan Beg <marijan.beg@xfel.eu>
2019-08-12 23:05:50 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='ubermagtable',
-    version='0.1.2',
+    version='0.1.3',
     description=('Python package for reading and merging OOMMF .odt '
                  'and mumax3 .txt files.'),
     long_description=long_description,
