6e65270c8d364ebe2554b9783fdb615b1670f076
Marijan Beg <m.beg@soton.ac.uk>
2020-03-06 19:31:12 +0000

    Update repo files.

--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,3 @@
-.. oommfodt documentation master file, created by
-   sphinx-quickstart on Fri Feb  1 14:48:21 2019.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 ubermagtable
 ============
 
--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open('README.md', encoding='utf-8') as f:
 setuptools.setup(
     name='ubermagtable',
     version='0.1.6',
-    description=('Python package for reading and merging OOMMF .odt '
-                 'and mumax3 .txt files.'),
+    description=('Python package for manipulating '
+                 'OOMMF and mumax3 tabular data.'),
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://ubermag.github.io',
     author=('Marijan Beg, Vanessa Nehruji, Sergii Mamedov, '
             'Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr'),
     packages=setuptools.find_packages(),
-    include_package_data = True,
+    include_package_data=True,
     install_requires=['pandas',
                       'numpy',
                       'pytest'],
