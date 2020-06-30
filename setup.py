827444d7c0e163b282c11fb0a5e4fdf61d1da741
Marijan Beg <m.beg@soton.ac.uk>
2020-06-30 18:14:02 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='ubermagtable',
-    version='0.1.11',
+    version='0.1.12',
     description=('Python package for manipulating '
                  'OOMMF and mumax3 tabular data.'),
     long_description=long_description,
