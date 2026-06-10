38fb1f73042dcf6ad3851e671ceb6b548410ef06
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-10 12:58:54 +0200

    Update test

--- ubermagtable/tests/test_interact.py
+++ ubermagtable/tests/test_interact.py
@@ -1,15 +1,8 @@
-import os
-
-import pytest
-
 import ubermagtable as ut
 
 
-@pytest.mark.skip
 def test_interact():
-    dirname = os.path.join(os.path.dirname(__file__), "test_sample/")
-    odtfile = os.path.join(dirname, "oommf-new-file2.odt")
-    table = ut.Table.fromfile(odtfile, x="t")
+    table = ut.sample_data()
 
     # Only test whether it runs.
     @ut.interact(xlim=table.slider())
